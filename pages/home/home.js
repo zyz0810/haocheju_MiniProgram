71dd91c2ed18a4d541366fa0aefadedbbeb4e5de
郑艳子 <zhengyanzi@tiaohuo.com>
2019-01-04 18:02:00 +0800

    首页swiper

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,15 +11,7 @@ Page(Object.assign({}, swiperAutoHeight, {
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
-    indicatorDots: true,
-    autoplay: false,
-    interval: 5000,
-    duration: 1000
+
   },
   // onReady() {
   //   this.setData({
